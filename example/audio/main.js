68b7f5adb6f3297ec151cb994c5af9b872db6d47
tommedema <tommedema@gmail.com>
2011-07-08 20:08:57 +0200

    added input song and example 1

---
+++ example/audio/main.js
@@ -0,0 +1,46 @@
+//NOTE: THIS EXAMPLE REQUIRED LIBVORBIS TO BE INSTALLED AND FFMPEG TO BE CONFIGURED WITH IT
+
+var ffmpeg = require('../../src/basicFFmpeg.js'),
+    util = require('util'),
+    fs = require('fs');
+
+//create input stream
+var inputStream = fs.createReadStream('../../assets/inputSong.mp3');
+
+//create output stream
+var outputStream = fs.createWriteStream('./outputSong.ogg');
+
+var processor = 
+    ffmpeg.createProcessor({
+        inputStream: inputStream //read from readable stream
+      , outputStream: outputStream //write to writable stream
+      , informInputAudioCodec: true //inputAudioCodec event will not be fired if this is not set to true
+      , fireInfoEvents: true //info events will not be fired if this is not set to true
+      , informProgress: true //progress events will not be fired if this is not set to true
+      , niceness: 10 //set child process niceness to 10
+      , timeout: 10 * 60 * 1000 //fire timeout event after 10 minutes, does not actually stop process
+      , arguments: { //the arguments passed, no syntatic sugar here (ffmpeg can be used just like its documentation says)
+            '-ab': '128k'
+          , '-acodec': 'libvorbis'
+        }
+    })
+    .on('info', function (infoChunk) {
+        util.debug('some info has been written to stderr: ' + infoChunk);
+    })
+    .on('inputAudioCodec', function (codec) {
+        util.debug('input audio codec is: ' + codec);
+    })
+    .on('success', function (retcode, signal) {
+        util.debug('process finished with retcode: ' + retcode);
+    })
+    .on('failure', function (retcode, signal) {
+        util.debug('the following error occured: ' + err);
+    })
+    .on('progress', function (bytes) {
+        util.debug('process event, bytes: ' + bytes);
+    })
+    .on('timeout', function (processor) {
+        util.debug('timeout event fired, stopping process.');
+        processor.terminate();
+    })
+    .execute();
