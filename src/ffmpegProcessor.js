845689cf725d03249f29f85a2f9ebd173c294984
tommedema <tommedema@gmail.com>
2011-07-08 17:27:12 +0200

    added timeout

--- src/ffmpegProcessor.js
+++ src/ffmpegProcessor.js
@@ -1,8 +1,15 @@
 //executes the given processor, which has options and state
 var executeProcessor = function (processor) {
     //check if processor is not already active
+    if (processor.state.childProcess) throw 'processor.execute called while processor has already been executed';
     
-    //set timeout
+    //set timeout event if applicable
+    if (processor.options.timeout) {
+        processor.state.timeoutTimer = setTimeout(function() {
+            processor.state.timeoutTimer = null;
+            processor.emit('timeout', processor);
+        }, processor.options.timeout);
+    }
     
     //create new child process with given inputStream and outputStream, set encodings, update state
     
