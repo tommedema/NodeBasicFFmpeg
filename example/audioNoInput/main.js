32d1f401eda948643a7651720ad85d1d1a8d746b
tommedema <tommedema@gmail.com>
2011-07-11 13:07:11 +0200

    added no input stream example

---
+++ example/audioNoInput/main.js
@@ -0,0 +1,79 @@
+//NOTE: THIS EXAMPLE REQUIRES LIBVORBIS TO BE INSTALLED AND FFMPEG TO BE CONFIGURED WITH IT
+
+//references
+var ffmpeg = require('../../src/basicFFmpeg'),
+    util = require('util'),
+    fs = require('fs');
+
+//create input stream
+var inputStream = 
+    fs.createReadStream('../../assets/inputSong.mp3')
+    .on('error', function (exception) {
+        util.debug('input stream exception: ' + exception);
+    })
+    .on('close', function () {
+        util.debug('input stream close');
+    });
+
+//create output stream
+var outputStream = 
+    fs.createWriteStream('./outputSong.ogg')
+    .on('error', function (exception) {
+        util.debug('output stream error: ' + exception);
+    })
+    .on('close', function () {
+        util.debug('output stream close');
+    })
+    .on('pipe', function() {
+        util.debug('a readable stream is now piping to output stream');
+    });
+
+//create a new processor with options, listen for events and execute
+var processor = 
+    ffmpeg.createProcessor({
+        outputStream: outputStream //write to writable stream
+      , emitInputAudioCodecEvent: true //inputAudioCodec event will not be fired if this is not set to true
+      , emitInfoEvent: true //info events will not be fired if this is not set to true
+      , emitProgressEvent: true //progress events will not be fired if this is not set to true
+      , niceness: 10 //set child process niceness to 10
+      , timeout: 10 * 60 * 1000 //fire timeout event after 10 minutes, does not actually stop process
+      , arguments: { //the arguments passed, no syntatic sugar here (ffmpeg can be used just like its documentation says)
+            '-ab': '128k'
+          , '-acodec': 'libvorbis'
+          , '-f': 'ogg'
+        }
+    })
+    .on('info', function (infoLine) {
+        util.log(infoLine);
+    })
+    .on('inputAudioCodec', function (codec) {
+        util.debug('input audio codec is: ' + codec);
+    })
+    .on('success', function (retcode, signal) {
+        util.debug('process finished successfully with retcode: ' + retcode + ', signal: ' + signal);
+    })
+    .on('failure', function (retcode, signal) {
+        util.debug('process failure, retcode: ' + retcode + ', signal: ' + signal);
+    })
+    .on('progress', function (bytes) {
+        util.debug('process event, bytes: ' + bytes);
+    })
+    .on('timeout', function (processor) {
+        util.debug('timeout event fired, stopping process.');
+        processor.terminate(); //note that this will still cause 'failure' to emit, since the process was not successful
+    })
+    .execute();
+
+inputStream
+    .on('data', function(chunk) {
+        if (!processor.options.inputStream.write(chunk)) { //write chunk to ffmpeg processor, returns false if kernel buffer is full
+            inputStream.pause(); //pause parsing and incoming data while we cannot send more to the receiving end
+            processor.options.inputStream.once('drain', function() {
+                inputStream.resume();
+            }); //resume when kernel buffer is empty again and we can write to receiving end
+        }
+    })
+    .on('end', function () {
+        util.debug('input stream end');
+        processor.options.inputStream.end();
+    });
