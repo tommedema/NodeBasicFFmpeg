5ab1231d23e0ab5adf251949482ff3be9bb2e447
tommedema <tommedema@gmail.com>
2011-08-02 12:28:16 +0200

    created bypass example

---
+++ example/MP3toMP3Bypass/main.js
@@ -0,0 +1,80 @@
+//NOTE: THIS EXAMPLE REQUIRES LIBMP3LAME TO BE INSTALLED AND FFMPEG TO BE CONFIGURED WITH IT
+
+//references
+var ffmpeg = require('../../src/basicFFmpeg'),
+    util = require('util'),
+    fs = require('fs');
+
+//create input stream
+var inputStream = 
+    fs.createReadStream('../../assets/inputSong.mp3')
+    .on('end', function () {
+        util.debug('input stream end');
+    })
+    .on('error', function (exception) {
+        util.debug('input stream exception: ' + exception);
+    })
+    .on('close', function () {
+        util.debug('input stream close');
+    });
+
+//create output stream
+var outputStream = 
+    fs.createWriteStream('./outputSong.mp3')
+    .on('error', function (exception) {
+        util.debug('output stream error: ' + exception);
+    })
+    .on('close', function () {
+        util.debug('output stream close');
+    })
+    .on('pipe', function() {
+        util.debug('a readable stream is now piping to output stream');
+    });
+
+//create a new processor with options, listen for events and execute
+var processor = 
+    ffmpeg.createProcessor({
+        inputStream: inputStream //read from readable stream
+      , outputStream: outputStream //write to writable stream
+      , emitInputAudioCodecEvent: true //inputAudioCodec event will not be fired if this is not set to true
+      , emitInfoEvent: true //info events will not be fired if this is not set to true
+      , emitProgressEvent: true //progress events will not be fired if this is not set to true
+      , niceness: 10 //set child process niceness to 10
+      , timeout: 10 * 60 * 1000 //fire timeout event after 10 minutes, does not actually stop process
+      , arguments: { //the arguments passed, no syntatic sugar here (ffmpeg can be used just like its documentation says)
+            '-ab': '128k'
+          , '-acodec': 'libmp3lame'
+          , '-f': 'mp3'
+        }
+    })
+    .on('info', function (infoLine) {
+        util.log(infoLine);
+    })
+    .on('inputAudioCodec', function (codec) {
+        util.debug('input audio codec is: ' + codec);
+        
+        //bypass if output == input
+        if (codec == processor.options.arguments['-f']) {
+            util.debug('bypassing because output == input!');
+            if (processor.bypass) {
+                processor.bypass();
+            }
+            else {
+                util.debug('cannot bypass because not set');
+            }
+        }
+    })
+    .on('success', function (retcode, signal) {
+        util.debug('process finished successfully with retcode: ' + retcode + ', signal: ' + signal);
+    })
+    .on('failure', function (retcode, signal) {
+        util.debug('process failure, retcode: ' + retcode + ', signal: ' + signal);
+    })
+    .on('progress', function (bytes) {
+        util.debug('process event, bytes: ' + bytes);
+    })
+    .on('timeout', function (processor) {
+        util.debug('timeout event fired, stopping process.');
+        processor.terminate(); //note that this will still cause 'failure' to emit, since the process was not successful
+    })
+    .execute();
